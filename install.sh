9d71272773ec54f7cb1347b9b0472024e0228d00
Mikołaj Biel <biel.mikolaj@openmailbox.org>
2017-11-12 13:22:54 +0100

    tag 0.1-specific changes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-sudo curl -o /usr/local/bin/agh-git https://raw.githubusercontent.com/klausweiss/agh-git/master/agh-git 
+sudo curl -o /usr/local/bin/agh-git https://raw.githubusercontent.com/klausweiss/agh-git/0.1/agh-git 
 sudo chmod +x /usr/local/bin/agh-git
