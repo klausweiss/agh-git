1f41be4c8774eece4683c90134cf58529c87e4f4
Mikołaj Biel <biel.mikolaj@openmailbox.org>
2017-11-12 11:26:05 +0100

    change order of parameters

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-sudo curl -o /usr/local/bin/agh-git https://raw.githubusercontent.com/klausweiss/agh-git/master/agh-git
+#!/bin/sh
+set -e
+sudo curl -o /usr/local/bin/agh-git https://raw.githubusercontent.com/klausweiss/agh-git/master/agh-git 
+sudo chmod +x /usr/local/bin/agh-git
